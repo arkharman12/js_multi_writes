9d45dfe8412a8774b2742f950efb836d293c83ba
Harman Ark <singhh@s-singhh.attlocal.net>
2020-05-09 16:55:14 -0400

    added helloworld.js

---
+++ helloWorld.js
@@ -0,0 +1,16 @@
+/**********
+ TITLE: jQuery							
+ AUTHOR: Harmanjot Singh (HS)
+ CREATE DATE: 16 Jan 2019
+ PURPOSE: Show a pop alert window with kind of message
+ LAST MODIFIED ON: 16 Jan 2019
+ LAST MODIFIED BY: Harmanjot Singh (HS)
+ MODIFICATION HISTORY: Original Build
+**********/
+
+$(document).ready(function(){
+			
+	// Pop up a window with a message
+	alert("Arg, ahoy JavaScript!");
+				
+}); // end of $(document).ready()
